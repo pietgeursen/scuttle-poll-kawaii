fd74e7e8cbd4fe81d657ba76ab52cb6794e6acc4
Piet Geursen <pietgeursen@gmail.com>
2018-08-04 15:41:06 +1200

    Refactor out into views

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,29 @@
 var express = require('express')
 var router = express.Router()
-var h = require('hyperscript')
 var pull = require('pull-stream')
 
 var render = require('../views/')
+var Polls = require('../views/polls')
 
 function createRouter (sbot, poll) {
   return router.get('/', function (req, res, next) {
     pull(
       // TODO: make the query filter not blow up the poll.poll.pull. thing
       poll.poll.pull[req.query.filter || 'all']({reverse: true, live: false}),
-      pull.asyncMap(function (msg, cb) {
-        poll.poll.async.get(msg.key, cb)
-      }),
-      pull.collect(function (err, polls) {
-        var content = h('div.Polls',
-          [
-            h('h1', 'Polls'),
-            h('span', [
-              Link('all'),
-              Link('open'),
-              Link('closed'),
-              Link('mine'),
-            ]),
-            h('div.polls', polls.map(Poll))
-          ]
-        )
-        res.send(render(content))
-      })
+      pull.filter(poll.poll.sync.isPoll),
+      pull.asyncMap(getPoll),
+      pull.collect(renderPolls)
     )
-
-    function Link (name) {
-      const filter = req.query.filter || 'all'
-      return h('a', {
-        href: `/polls?filter=${name}`,
-        className: filter === name ? '-active' : '' },
-        name
-      )
+    function renderPolls (err, polls) {
+      // if (err) { return 500 }
+      console.log(err)
+      res.send(render(Polls(polls)))
     }
   })
-}
 
-function Poll (poll) {
-  return h('div.Poll', [
-    h('div.title', poll.title)
-  ])
+  function getPoll (msg, cb) {
+    poll.poll.async.get(msg.key, cb)
+  }
 }
 
 module.exports = createRouter
---
+++ views/polls.js
@@ -0,0 +1,31 @@
+var h = require('hyperscript')
+
+module.exports = function Polls (polls, filter) {
+  return h('div.Polls',
+    [
+      h('h1', 'Polls'),
+      h('span', [
+        Link('all'),
+        Link('open'),
+        Link('closed'),
+        Link('mine')
+      ]),
+      h('div.polls', polls.map(PollSummary))
+    ]
+  )
+}
+
+function Link (name, filter) {
+  filter = filter || 'all'
+  return h('a', {
+    href: `/polls?filter=${name}`,
+    className: filter === name ? '-active' : ''
+  },
+  name
+  )
+}
+function PollSummary (poll) {
+  return h('div.Poll', [
+    h('div.title', poll.title)
+  ])
+}
